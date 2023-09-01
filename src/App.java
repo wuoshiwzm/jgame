091bba0d9288a887112fdba3fea24e46ca5e92d2
wuoshiwzm <wuoshiwzm@hotmail.com>
2023-09-01 22:07:54 +0800

    Create App.java

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import com.leon.Game;
+import com.leon.Login;
+import com.leon.Register;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        new Login();
+
+        new Game();
+
+        new Register();
+
+    }
+
+}
