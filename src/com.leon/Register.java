cfd6021c56ed3f70c4845320658109b157ce549d
wuoshiwzm <wuoshiwzm@hotmail.com>
2023-09-01 22:08:58 +0800

    Create Register.java

---
+++ src/com.leon/Register.java
@@ -0,0 +1,12 @@
+package com.leon;
+
+import javax.swing.*;
+
+public class Register extends JFrame {
+
+    public Register(){
+        this.setTitle("Register page");
+        this.setSize(480, 600);
+        this.setVisible(true);
+    }
+}
