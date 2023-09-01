f97403f54bb95bfa48a56c8d3a40c8227cb9886d
wuoshiwzm <wuoshiwzm@hotmail.com>
2023-09-01 22:08:41 +0800

    Create Login.java

---
+++ src/com.leon/Login.java
@@ -0,0 +1,12 @@
+package com.leon;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+
+    public Login(){
+        this.setTitle("Login page");
+        this.setSize(480, 430);
+        this.setVisible(true);
+    }
+}
